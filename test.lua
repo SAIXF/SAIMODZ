ec3f4bcb0677555411d3e877d550b462c5da4dbd
SAIXF <118271066+SAIXF@users.noreply.github.com>
2022-12-12 12:17:52 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,9 +1,20 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("TITLE", "DarkTheme")
+local Window = Library.CreateLib("UI FREE", "DarkTheme")
 
 local Tab = Window:NewTab("Main")
 local Section = Tab:NewSection("Farm")
 
 Section:NewButton("ButtonText", "ButtonInfo", function()
     print("Clicked")
+end)
+
+local Tab = Window:NewTab("Main2")
+local Section = Tab:NewSection("Farm2")
+
+Section:NewToggle("ToggleText", "ToggleInfo", function(state)
+    if state then
+        print("Toggle On")
+    else
+        print("Toggle Off")
+    end
 end)
